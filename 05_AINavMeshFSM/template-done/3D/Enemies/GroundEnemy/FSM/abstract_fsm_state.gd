bd466f7ce5b35af6413449282f8e0ba97dac5d02
tutek1 <ondrej@kyzr.cz>
2025-07-15 16:27:31 +0200

    Lab05 - Added unfilled template-done

---
+++ 05_AINavMeshFSM/template-done/3D/Enemies/GroundEnemy/FSM/abstract_fsm_state.gd
@@ -0,0 +1,14 @@
+class_name AbstractFSMState
+extends Resource
+
+# Called upon transition into the state
+func state_entered(_enemy : GroundEnemyFSM) -> void:
+	assert(false, "Do not use this abstract class!")
+
+# Called every physics process frame
+func state_physics_process(_enemy : GroundEnemyFSM, _delta : float) -> void:
+	assert(false, "Do not use this abstract class!")
+
+# Called upon transition from the state
+func state_exit(_enemy : GroundEnemyFSM) -> void:
+	assert(false, "Do not use this abstract class!")
---
+++ 05_AINavMeshFSM/template-done/3D/Enemies/GroundEnemy/FSM/ground_enemy_fsm.gd
@@ -0,0 +1,57 @@
+class_name GroundEnemyFSM
+extends CharacterBody3D
+
+@export var speed : float = 5.0
+@export var up_speed : float = 5.0
+@export var rotation_speed : float = 7
+
+
+
+func _physics_process(delta : float) -> void:
+	_gravity(delta)
+	_movement()
+	_rotate_enemy(delta, velocity)
+	
+	move_and_slide()
+
+# Handles falling
+func _gravity(delta : float) -> void:
+	if is_on_floor(): return
+	
+	velocity += get_gravity() * delta
+
+# Handles movement of the player
+func _movement() -> void:
+	# TEMPORARY until navmesh setup
+	var target_pos : Vector3 = global_position + Vector3.FORWARD
+	
+	var direction : Vector3 = target_pos - global_position
+	direction = direction.normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+
+# Rotates the enemy based on direction and delta
+func _rotate_enemy(delta: float, direction : Vector3) -> void:
+	if Vector2(direction.x, direction.z).length() < 0.1: return
+	var angle : float = atan2(direction.x, direction.z) - PI
+	
+	rotation.y = lerp_angle(rotation.y, angle, rotation_speed * delta)
+
+# DEBUG function to test out the navmesh
+func _input(event : InputEvent):
+	if event is InputEventMouseButton:
+		
+		# Get mouse and create raycast ray
+		var mouse_pos : Vector2 = get_viewport().get_mouse_position()
+		var ray_origin : Vector3 = get_viewport().get_camera_3d().project_ray_origin(mouse_pos)
+		var ray_direction : Vector3 = get_viewport().get_camera_3d().project_ray_normal(mouse_pos)
+		var ray_end = ray_origin + ray_direction * 1000.0
+		
+		# Query the physics server if we collided
+		var space_state = get_world_3d().direct_space_state
+		var query = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+		var result : Dictionary = space_state.intersect_ray(query)
+		
+		if result:
+			# TODO go there
+			pass
